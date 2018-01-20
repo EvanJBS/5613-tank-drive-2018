9862139e1783a92e850d6d6ec244a742116f8460
Evan <evan.bryan-sing@cmsbears.org>
2018-01-20 14:50:12 -0700

    rewrote code for four motors

--- robot.py
+++ robot.py
@@ -4,45 +4,29 @@ import wpilib
 from wpilib.drive import DifferentialDrive
 
 
-class MyRobot(wpilib.IterativeRobot):
+class MyRobot(wpilib.SampleRobot):
 
     def robotInit(self):
+        self.frontLeft = wpilib.Spark(1)
+        self.rearLeft = wpilib.Spark(2)
+        self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
 
-        # object that handles basic drive operations
-        self.frontLeftMotor = wpilib.Spark(0)
-        self.rearLeftMotor = wpilib.Spark(1)
-        self.frontRightMotor = wpilib.Spark(2)
-        self.rearRightMotor = wpilib.Spark(3)
+        self.frontRight = wpilib.Spark(3)
+        self.rearRight = wpilib.Spark(4)
+        self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
 
-        self.left = wpilib.SpeedControllerGroup(self.frontLeftMotor, self.rearLeftMotor)
-        self.right = wpilib.SpeedControllerGroup(self.frontRightMotor, self.rearRightMotor)
+        self.drive = DifferentialDrive(self.left, self.right)
 
-        self.myRobot = DifferentialDrive(self.left, self.right)
-        self.myRobot.setExpiration(0.1)
+    def autonomous(self):
+        timer = wpilib.Timer()
+        timer.start()
+        while self.isAutonomous() and self.isEnabled():
+            if timer.get() < 3.0:
+                self.drive.tankDrive(-1, -1)
+            else:
+                self.drive.tankDrive(0, 0)
 
-        # joysticks 1 & 2 on the driver station
-        self.leftStick = wpilib.Joystick(0)
-        self.rightStick = wpilib.Joystick(1)
 
-    def teleopInit(self):
-        '''Executed at the start of teleop mode'''
-        self.myRobot.setSafetyEnabled(True)
-
-    def teleopPeriodic(self):
-        '''Runs the motors with tank steering'''
-        self.myRobot.tankDrive(self.leftStick.getY() * -1, self.rightStick.getY() * -1)
-
-    def autonomousInit(self):
-        self.auto_loop_counter = 0
-
-    def autonomousPeriodic(self):
-
-        if self.auto_loop_counter < 100:
-            self.myRobot.tankDrive(-0.5, 0)
-            self.auto_loop_counter += 1
-
-        else:
-            DifferentialDrive(0, 0)
 
 
 if __name__ == '__main__':
