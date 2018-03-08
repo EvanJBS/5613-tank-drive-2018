a1edf93bde294d5667561cb564d519134efb538b
RustyRaptor <ziadarafat1@gmail.com>
2018-03-08 11:30:32 -0700

    Restructured Teleop

--- robot.py
+++ robot.py
@@ -2,11 +2,18 @@
 
 import wpilib
 from wpilib.drive import DifferentialDrive
+from robotpy_ext.control.toggle import Toggle
 
 
 class MyRobot(wpilib.IterativeRobot):
 
     def robotInit(self):
+        self.DriveSpd = 1
+        self.RotationSpd = 1
+        self.IntakeSpd = 0
+        self.ShooterSpd = 1
+        self.Direction = 1
+
         self.frontLeft = wpilib.Spark(0)
         self.rearLeft = wpilib.Spark(1)
         self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
@@ -15,61 +22,80 @@ class MyRobot(wpilib.IterativeRobot):
         self.rearRight = wpilib.Spark(3)
         self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
 
-        self.intake = wpilib.Spark(4)
+        self.drive = DifferentialDrive(self.left, self.right)
+
+        self.stick1 = wpilib.Joystick(0)
+        self.stick2 = wpilib.Joystick(1)
+        # self.toggle7 = Toggle(self.stick1, 7)
+        # self.toggle1 = Toggle(self.stick1, 1)
+        # self.toggle6 = Toggle(self.stick1, 6)
+        # self.toggle4 = Toggle(self.stick1, 4)
+        # self.toggle5 = Toggle(self.stick1, 5)
+        # self.toggle3 = Toggle(self.stick1, 3)
+        # self.toggle2 = Toggle(self.stick1, 2)
+
+        self.IntakeSpd = wpilib.Spark(4)
         self.Lshoot = wpilib.Spark(5)
         self.Rshoot = wpilib.Spark(6)
         self.shoot = wpilib.SpeedControllerGroup(self.Lshoot, self.Rshoot)
 
-
-        self.drive = DifferentialDrive(self.left, self.right)
-        self.stick1 = wpilib.Joystick(0)
-        self.stick2 = wpilib.Joystick(1)
-
     def disabled(self):
-        while self.isDisabled():
-            wpilib.Timer.delay(0.01)
+        wpilib.Timer.delay(0.01)
+
     def teleopInit(self):
-        timer = wpilib.Timer()
-        timer.start()
-        self.a = -1
-        self.b = 0
-        self.c = 0
+        self.timer = wpilib.Timer()
+        self.timer.start()
+
     def teleopPeriodic(self):
-        self.drive.arcadeDrive(self.stick1.getY() * self.a, self.stick1.getX() * 0.8)
-        self.intake.set(self.b)
-        self.shoot.set(self.c)
-        if self.stick1.getRawButton(7) and self.stick1.getRawButton(1):
-            self.a = 0.6
-        elif self.stick1.getRawButton(7):
-            self.a = -0.6
-        elif self.stick1.getRawButton(1):
-            self.a = 1
-        elif self.stick1.getRawButton(6):
-            self.b = 1
+        self.drive.arcadeDrive((self.stick1.getY() * self.DriveSpd) * self.Direction, self.stick1.getX() * self.RotationSpd)
+        self.IntakeSpd.set(self.IntakeSpd)
+        self.shoot.set(self.ShooterSpd * self.ShooterSpd)
+
+        if self.stick1.getRawButton(1):
+            self.Direction = -1
+        else:
+            self.Direction = 1
+        if self.stick1.getRawButton(2):
+            self.DriveSpd = 0.5
+        else:
+            self.DriveSpd = 1
+        if self.stick1.getRawButton(6):
+            self.IntakeSpd = 1
         elif self.stick1.getRawButton(4):
-            self.b = -1
-        elif self.stick1.getRawButton(5):
-            self.c = 1
+            self.IntakeSpd = -1
+        else:
+            self.IntakeSpd = 0
+        if self.stick1.getRawButton(5):
+            self.ShooterSpd = 1
         elif self.stick1.getRawButton(3):
-            self.c = -1
-        elif self.stick1.getRawButton(2):
-            self.b = 1
-            self.c = 1
+            self.ShooterSpd = -1
         else:
-            self.a = -1
-            self.b = 0
-            self.c = 0
+            self.ShooterSpd = 0
+
+        self.timer.delay(0.02)
     def autonomousInit(self):
-        timer = wpilib.Timer()
-        timer.start()
-        if wpilib.DriverStation.getInstance().getGameSpecificMessage() == "RRR" or "RLR":
-            self.speed = 1
-        elif wpilib.DriverStation.getInstance().getGameSpecificMessage() == "LLL" or "LRL":
-            self.speed = -1
-        else:
-            self.speed = 0
+        self.timer = wpilib.Timer()
+        self.timer.start()
+        self.gamedata = wpilib.DriverStation.getInstance().getGameSpecificMessage()
+        print(self.gamedata)
+
     def autonomousPeriodic(self):
-        self.drive.arcadeDrive(self.speed, 0)
+        self.drive.arcadeDrive(self.Speed * self.Direction, self.Rotation)
+        self.IntakeSpd.set(self.IntakeSpd)
+        self.shoot.set(self.ShooterSpd)
+        if self.gamedata is "LLL" or "LRL" and not "RRR" or "RLR":
+            if self.timer.get() < 1.0:
+                self.Speed = 0.5
+            else:
+                self.Speed = 0
+        if self.gamedata is "RRR" or "RLR" and not "LLL" or "LRL":
+            if self.timer.get() < 1.0:
+                self.Speed = -0.5
+            else:
+                self.Speed = 0
+
+        else:
+            self.Speed = -1
 
 
 if __name__ == '__main__':
